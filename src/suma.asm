400250d3df206dcd09bc2cc9549652398c7b847d
Sir-Albert <edgar.betancourt@uabc.edu.mx>
2026-03-19 01:44:26 +0000

    p5

--- src/suma.asm
+++ src/suma.asm
@@ -1,20 +1,17 @@
-section .data
-    msg db "Hola Mundo", 10
-    len equ $ - msg
+%include "../lib/pc_io.inc"  	; incluir declaraciones de procedimiento externos
+								; que se encuentran en la biblioteca libpc_io.a
 
-section .text
-    global _start
+section	.text
+	global _start       ;referencia para inicio de programa
+	
+_start:                   
+	mov edx, msg		; edx = dirección de la cadena msg
+	call puts			; imprime cadena msg terminada en valor nulo (0)
 
-_start:
-    mov eax,10h
-    ; write(stdout, msg, len)
-    mov rax, 1      ; syscall write
-    mov rdi, 1      ; descriptor stdout
-    mov rsi, msg    ; direccion del mensaje
-    mov rdx, len    ; longitud
-    syscall
+	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
+	int	0x80        	; llamada al sistema - fin de programa
 
-    ; exit(0)
-    mov rax, 60     ; syscall exit
-    xor rdi, rdi    ; codigo de salida 0
-    syscall
+section	.data
+    msg	db  'abcdefghijklmnopqrstuvwxyz0123456789LOL',0xa,0 
+
+
